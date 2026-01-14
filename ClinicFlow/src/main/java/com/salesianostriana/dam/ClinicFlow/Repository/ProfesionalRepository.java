f8dd35a9f0a0e8bd74d13707d84530a573243eca
Jesús Zamorano Rodríguez <71210859+Jesus11025@users.noreply.github.com>
2026-01-14 09:49:40 +0100

    Basics Repositorys finished

---
+++ ClinicFlow/src/main/java/com/salesianostriana/dam/ClinicFlow/Repository/ProfesionalRepository.java
@@ -0,0 +1,7 @@
+package com.salesianostriana.dam.ClinicFlow.Repository;
+
+import com.salesianostriana.dam.ClinicFlow.Models.Profesional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfesionalRepository extends JpaRepository<Profesional, Long> {
+}
